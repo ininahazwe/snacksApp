6a95b484b97433f909bb61af19c1c7353a0b25c7
ininahazwe <yves.cri@gmail.com>
2026-06-16 16:59:28 +0000

    ajout crud stock et produit et client

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-85ec4f4b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bg93tb96es"
+    "revision": "0.add8s8sbj7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
